c0b581b2e069980c5d2b868a78f038f27df0d679
val martinchik <valmartsin@gmail.com>
2018-07-11 11:48:49 +0300

    initial project created commit

---
+++ Lykke.Logs.MsSql/Lykke.Logs.MsSql/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lykke.Logs.MsSql
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
